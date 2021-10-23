4584a43498b944a9f3c2253297b850d3074bc3e0
ilyal <ilyallifsh@gmail.com>
2021-10-23 14:40:24 +0300

    adds corrections for HW_5 after YG and VR

---
+++ src/telran/util/ListTest.java
@@ -0,0 +1,72 @@
+package telran.util;
+//HW_5 Ilya_L after corrections 23.10.21
+ 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ListTest {
+private List<Integer> numbers;
+private List<String> strings;
+Integer initialNumbers[] = {10, 20, 40};
+String initialStrings[] = {"name1", "name2"};
+	@BeforeEach
+	void setUp() throws Exception {
+		numbers = getInitialNumbers();
+		strings = getInitialStrings();
+	}
+
+	private List<String> getInitialStrings() {
+		List<String> res = new ArrayList<>();
+		for (int i = 0; i < initialStrings.length; i++) {
+			res.add(initialStrings[i]);
+		}
+		return res;
+	}
+
+	private List<Integer> getInitialNumbers() {
+		List<Integer> res = new ArrayList<>(1);
+		for (int i = 0; i < initialNumbers.length; i++) {
+			res.add(initialNumbers[i]);
+		}
+		return res;
+	}
+
+	@Test
+	void testGet() {
+		assertEquals(10, numbers.get(0));
+		assertEquals("name1", strings.get(0));
+		assertEquals(null, numbers.get(-1));
+		assertEquals(null, numbers.get(3));
+	}
+	
+	@Test
+	void testAddIndex() {
+		assertEquals(true, strings.add(0, "name3"));
+		assertEquals("name3", strings.get(0));
+		assertEquals(false, strings.add(100, "name3"));
+		assertEquals(false, strings.add(-1, "name3"));
+		assertEquals(true, strings.add(3, "name3"));
+		
+// test allocate method		
+		for (int i = 0; i < 20; i++) {
+			assertEquals(true, strings.add(i, "name" + i));
+			assertEquals("name" + i, strings.get(i));
+		}
+// checking that when adding a new element to the array, the all of the element values are saved correctly
+		for (int i = 0; i < 20; i++) {
+			assertEquals("name" + i, strings.get(i));
+		}
+		 
+	}
+	
+	@Test
+	void testRemove() {
+		assertEquals(10, numbers.remove(0));
+		assertEquals(2, numbers.size());
+		assertEquals(null, numbers.remove(100));
+
+	}
+}
+
